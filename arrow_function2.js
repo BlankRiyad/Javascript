426bd0f1d5c662abfba24967d0f107127bb540e7
BlankRiyad <akandablank@gmail.com>
2022-01-08 18:25:14 +0600

    Shortest form of arrow function

---
+++ arrow_function2.js
@@ -0,0 +1,8 @@
+/* Shortest form of Arrow function. We can leave out the curly braces and return keyword
+in case of one statement inside the function.*/
+
+
+
+const getVehiclesCount = () => 400;
+
+console.log(getVehiclesCount());
